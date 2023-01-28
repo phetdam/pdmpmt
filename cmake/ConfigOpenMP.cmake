1f1f3e6bca3e3a1fb75eebdf5a77686979b0badc
Derek Huang <djh458@stern.nyu.edu>
2023-01-28 01:17:14 -0500

    ConfigOpenMP.cmake: use generator expression for OpenMP flags

--- cmake/ConfigOpenMP.cmake
+++ cmake/ConfigOpenMP.cmake
@@ -4,7 +4,10 @@ if(MSVC)
     # check OpenMP support. MSVC OpenMP support lags behind GCC's by a lot
     find_package(OpenMP 2.0)
     if(OpenMP_FOUND)
-        add_compile_options(${OpenMP_C_FLAGS} ${OpenMP_CXX_FLAGS})
+        add_compile_options(
+            $<$<COMPILE_LANGUAGE:C>:${OpenMP_C_FLAGS}>
+            $<$<COMPILE_LANGUAGE:CXX>:${OpenMP_CXX_FLAGS}>
+        )
     else()
         message(
             WARNING "OpenMP 2.0 not supported by MSVC version ${MSVC_VERSION}"
@@ -15,7 +18,10 @@ else()
     # check OpenMP support, GCC 9.x and clang 3.9+ implement 4.5
     find_package(OpenMP 4.5)
     if(OpenMP_FOUND)
-        add_compile_options(${OpenMP_C_FLAGS} ${OpenMP_CXX_FLAGS})
+        add_compile_options(
+            $<$<COMPILE_LANGUAGE:C>:${OpenMP_C_FLAGS}>
+            $<$<COMPILE_LANGUAGE:CXX>:${OpenMP_CXX_FLAGS}>
+        )
     else()
         message(
             WARNING "OpenMP not supported by
