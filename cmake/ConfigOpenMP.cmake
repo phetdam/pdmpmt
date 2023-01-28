44f90a97a06296fdd85bd819a882c9d15386b0c0
Derek Huang <djh458@stern.nyu.edu>
2023-01-28 01:28:01 -0500

    update OpenMP use and linkage for the C library
    cmake/ConfigOpenMP.cmake
        Only check for OpenMP availability, don't add compile flags
    
    src/CMakeLists.txt
        Use OpenMP::OpenMP_C target to indicate OpenMP dependency

--- cmake/ConfigOpenMP.cmake
+++ cmake/ConfigOpenMP.cmake
@@ -3,12 +3,7 @@ cmake_minimum_required(VERSION 3.21)
 if(MSVC)
     # check OpenMP support. MSVC OpenMP support lags behind GCC's by a lot
     find_package(OpenMP 2.0)
-    if(OpenMP_FOUND)
-        add_compile_options(
-            $<$<COMPILE_LANGUAGE:C>:${OpenMP_C_FLAGS}>
-            $<$<COMPILE_LANGUAGE:CXX>:${OpenMP_CXX_FLAGS}>
-        )
-    else()
+    if(NOT OpenMP_FOUND)
         message(
             WARNING "OpenMP 2.0 not supported by MSVC version ${MSVC_VERSION}"
         )
@@ -17,12 +12,7 @@ if(MSVC)
 else()
     # check OpenMP support, GCC 9.x and clang 3.9+ implement 4.5
     find_package(OpenMP 4.5)
-    if(OpenMP_FOUND)
-        add_compile_options(
-            $<$<COMPILE_LANGUAGE:C>:${OpenMP_C_FLAGS}>
-            $<$<COMPILE_LANGUAGE:CXX>:${OpenMP_CXX_FLAGS}>
-        )
-    else()
+    if(NOT OpenMP_FOUND)
         message(
             WARNING "OpenMP not supported by
   C compiler ${CMAKE_C_COMPILER} ${CMAKE_C_COMPILER_VERSION}
