374820862f7bf5f38a914c9bc7a5db2eacaefa69
Derek Huang <djh458@stern.nyu.edu>
2023-07-24 06:55:25 -0400

    add ConfigCUDA.cmake with CUDA Toolkit config + include in CMakeLists.txt
    CMakeLists.txt
        Include ConfigCUDA.cmake code
    
    cmake/ConfigCUDA.cmake
        Add CUDA Toolkit configuration

---
+++ cmake/ConfigCUDA.cmake
@@ -0,0 +1,26 @@
+cmake_minimum_required(VERSION ${CMAKE_MINIMUM_REQUIRED_VERSION})
+
+# minimum CUDA Toolkit version to use
+set(PDMPMT_CUDA_TOOLKIT_VERSION 11.7)
+find_package(CUDAToolkit ${PDMPMT_CUDA_TOOLKIT_VERSION})
+# if found, configure, else just warn that no CUDA files will be compiled
+if(CUDAToolkit_FOUND)
+    # same C++ standard and requirement
+    set(CMAKE_CUDA_STANDARD ${CMAKE_CXX_STANDARD})
+    set(CMAKE_CUDA_STANDARD_REQUIRED ${CMAKE_CXX_STANDARD_REQUIRED})
+    # not necessarily the same as CUDAToolkit_NVCC_EXECUTABLE, set manually
+    if(NOT CMAKE_CUDA_COMPILER)
+        set(CMAKE_CUDA_COMPILER ${CUDAToolkit_NVCC_EXECUTABLE})
+    endif()
+    message(
+        STATUS
+        "ConfigCUDA: Using CMAKE_CUDA_COMPILER ${CMAKE_CUDA_COMPILER}"
+    )
+    enable_language(CUDA)
+else()
+    message(
+        WARNING
+        "CUDA Toolkit ${PDMPMT_CUDA_TOOLKIT_VERSION} not found, \
+CUDA compilation will be skipped"
+    )
+endif()
