45d026ee59cf875be546dd804a1307304967ccc9
Derek Huang <djh458@stern.nyu.edu>
2024-12-11 09:16:48 -0500

    type_traits.hh: add is_uniform_random_bit_generator traits type

--- include/pdmpmt/type_traits.hh
+++ include/pdmpmt/type_traits.hh
@@ -48,6 +48,46 @@ constexpr bool is_ostreamable_v = is_ostreamable<T>::value;
 template <typename T>
 using ostreamable_t = std::enable_if_t<is_ostreamable_v<T>>;
 
+/**
+ * Traits type to model the *UniformRandomBitGenerator* named concept.
+ *
+ * @tparam T type
+ */
+template <
+  typename T,
+  typename = void,
+  typename = void,
+  typename = void,
+  typename = void>
+struct is_uniform_random_bit_generator : std::false_type {};
+
+/**
+ * True specialization for types that satisfy *UniformRandomBitGenerator*.
+ *
+ * @tparam T type
+ */
+template <typename T>
+struct is_uniform_random_bit_generator<
+  T,
+  // result_type member should be an unsigned type
+  std::enable_if_t<std::is_unsigned_v<typename T::result_type>>,
+  // min() and max() static functions should be defined and return result_type
+  std::enable_if_t<std::is_same_v<decltype(T::min()), typename T::result_type>>,
+  std::enable_if_t<std::is_same_v<decltype(T::max()), typename T::result_type>>,
+  // invocation returns result_type
+  std::enable_if_t<
+    std::is_same_v<decltype(std::declval<T>()()), typename T::result_type>
+  > > : std::true_type {};
+
+/**
+ * Indicate that a type satisfies *UniformRandomBitGenerator*.
+ *
+ * @tparam T type
+ */
+template <typename T>
+constexpr bool
+is_uniform_random_bit_generator_v = is_uniform_random_bit_generator<T>::value;
+
 }  // namespace pdmpmt
 
 #endif  // PDMPMT_TYPE_TRAITS_HH_
