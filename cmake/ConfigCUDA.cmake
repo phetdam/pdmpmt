de05c138ca3ce9b5c5f62d94048f2c11690ec7a0
Derek Huang <djh458@stern.nyu.edu>
2024-12-13 01:08:50 -0500

    ConfigCUDA.cmake: bump CUDA Toolkit minimuum version to 12 to allows C++20 usage

--- cmake/ConfigCUDA.cmake
+++ cmake/ConfigCUDA.cmake
@@ -1,7 +1,8 @@
 cmake_minimum_required(VERSION ${CMAKE_MINIMUM_REQUIRED_VERSION})
 
 # minimum CUDA Toolkit version to use
-set(PDMPMT_CUDA_TOOLKIT_VERSION 11.7)
+# note: we choose CUDA Toolkit 12 as then NVCC can support C++20
+set(PDMPMT_CUDA_TOOLKIT_VERSION 12.2)
 find_package(CUDAToolkit ${PDMPMT_CUDA_TOOLKIT_VERSION})
 # if found, configure, else just warn that no CUDA files will be compiled
 if(CUDAToolkit_FOUND)
