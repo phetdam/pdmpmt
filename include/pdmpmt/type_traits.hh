290bbe52e1798e78987127435d0ad4209124394e
Derek Huang <djh458@stern.nyu.edu>
2024-12-13 02:19:39 -0500

    type_traits.hh: add traits for indirectly readable, equality comparable, and pre-incrementable types

--- include/pdmpmt/type_traits.hh
+++ include/pdmpmt/type_traits.hh
@@ -10,6 +10,7 @@
 
 #include <iosfwd>
 #include <type_traits>
+#include <utility>
 
 namespace pdmpmt {
 
@@ -47,6 +48,82 @@ constexpr bool is_ostreamable_v = is_ostreamable<T>::value;
  */
 template <typename T>
 using ostreamable_t = std::enable_if_t<is_ostreamable_v<T>>;
+
+/**
+ * Traits helpers to indicate that a type is deferenceable.
+ *
+ * @tparam T type
+ */
+template <typename T, typename = void>
+struct is_indirectly_readable : std::false_type {};
+
+/**
+ * True specialization for types that are dereferenceable.
+ *
+ * @tparam T type
+ */
+template <typename T>
+struct is_indirectly_readable<T, std::void_t<decltype(*std::declval<T>())>>
+  : std::true_type {};
+
+/**
+ * Indicate that a type is dereferenceable.
+ *
+ * @tparam T type
+ */
+template <typename T>
+constexpr bool is_indirectly_readable_v = is_indirectly_readable<T>::value;
+
+/**
+ * Traits helper to indicate that a type is equality comparable.
+ *
+ * @tparam T type
+ */
+template <typename T, typename = void>
+struct is_equality_comparable : std::false_type {};
+
+/**
+ * True specialization for types that are equality comparable.
+ *
+ * @tparam T type
+ */
+template <typename T>
+struct is_equality_comparable<
+  T, std::void_t<decltype(std::declval<T>() == std::declval<T>())> >
+  : std::true_type {};
+
+/**
+ * Traits helper to indicate that a type is equality comparable.
+ *
+ * @tparam T type
+ */
+template <typename T>
+constexpr bool is_equality_comparable_v = is_equality_comparable<T>::value;
+
+/**
+ * Indicate that a type is pre-incrementable.
+ *
+ * @tparam T type
+ */
+template <typename T, typename = void>
+struct is_pre_incrementable : std::false_type {};
+
+/**
+ * True specialization for pre-incrementable types.
+ *
+ * @tparam T type
+ */
+template <typename T>
+struct is_pre_incrementable<T, std::void_t<decltype(++std::declval<T>())> >
+  : std::true_type {};
+
+/**
+ * Indicate that a type is pre-incrementable.
+ *
+ * @tparam T type
+ */
+template <typename T>
+constexpr bool is_pre_incrementable_v = is_pre_incrementable<T>::value;
 
 /**
  * Traits type to model the *UniformRandomBitGenerator* named concept.
