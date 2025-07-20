a5c641eb748a494223799ece8596754e73916baa
Derek Huang <djh458@stern.nyu.edu>
2025-07-19 22:04:49 -0400

    features.h: add PDMPMT_HAS_CXX20 and put PDMPMT_HAS_CC20 on life support

--- include/pdmpmt/features.h
+++ include/pdmpmt/features.h
@@ -13,12 +13,17 @@
 // C++20
 #ifdef PDMPMT_CPLUSPLUS
 #if PDMPMT_CPLUSPLUS >= 202002L
-#define PDMPMT_HAS_CC20 1
+#define PDMPMT_HAS_CXX20 1
 #endif  // PDMPMT_CPLUSPLUS >= 202002L
 #endif  // PDMPMT_CPLUSPLUS
 
-#ifndef PDMPMT_HAS_CC20
-#define PDMPMT_HAS_CC20 0
-#endif  // PDMPMT_HAS_CC20
+// TODO: use CXX macros and remove CC ones
+#ifndef PDMPMT_HAS_CXX20
+#define PDMPMT_HAS_CXX20 0
+#endif  // PDMPMT_HAS_CXX20
+
+// compatibility
+// TODO: refactor uses of the compatibility macros
+#define PDMPMT_HAS_CC20 PDMPMT_HAS_CXX20
 
 #endif  // PDMPMT_FEATURES_H_
