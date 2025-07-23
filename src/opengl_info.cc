fd54218f2dd311c01747e9900b69a8fedec00ca3
Derek Huang <djh458@stern.nyu.edu>
2025-07-23 04:00:18 -0400

    opengl_info.cc: add X11 + GLX section printing some version + extensions info

--- src/opengl_info.cc
+++ src/opengl_info.cc
@@ -14,11 +14,27 @@
 #include <wingdi.h>
 #endif  // _WIN32
 
+// TODO: make part of features.h
+#ifdef __has_include
+#if __has_include(<X11/Xlib.h>)
+#include <X11/Xlib.h>
+#define PDMPMT_HAS_X11 1
+#endif  // __has_include(<X11/Xlib.h>)
+#endif  // __has_include
+
+#ifndef PDMPMT_HAS_X11
+#define PDMPMT_HAS_X11 0
+#endif  // PDMPMT_HAS_X11
+
 #include <cstdlib>
 #include <filesystem>
 #include <iostream>
 
 #include <GL/gl.h>
+// TODO: should have separate check for GLX
+#if PDMPMT_HAS_X11
+#include <GL/glx.h>
+#endif  // PDMPMT_HAS_X11
 
 #include "pdmpmt/opengl.hh"
 #ifdef _WIN32
@@ -106,8 +122,58 @@ int main()
     // a large number of extensions for the device and it becomes very verbose
     "OpenGL extensions: " << pdmpmt::opengl::extensions << "\n" <<
     std::flush;
+#elif PDMPMT_HAS_X11
+  // open default X display (e.g. :0 on WSL1 from the DISPLAY env var)
+  auto dsp = XOpenDisplay(nullptr);
+  if (!dsp) {
+    // default display is from the DISPLAY env var. if nullptr, then unknown
+    auto dsp_name = []
+    {
+      auto name = std::getenv("DISPLAY");
+      return (name) ? name : "(unknown)";
+    }();
+    std::cerr << "Error: Unable to open default display " << dsp_name <<
+      std::endl;
+    return EXIT_FAILURE;
+  }
+  // default screen number
+  auto scn = DefaultScreen(dsp);
+  // print display info
+  std::cout <<
+    "X display: " << DisplayString(dsp) << "\n" <<
+    "X protocol: " << ProtocolVersion(dsp) << "." << ProtocolRevision(dsp) <<
+      "\n" <<
+    "X vendor: " << ServerVendor(dsp) << "\n" <<
+    "X screen: " << scn << "\n" <<
+    std::flush;
+  // check if GLX is supported by the X display
+  // TODO: what are the GLX error/event bases?
+  if (!glXQueryExtension(dsp, nullptr, nullptr)) {
+    std::cerr << "Error: X display " << DisplayString(dsp) <<
+      " does not support GLX" << std::endl;
+    return EXIT_FAILURE;
+  }
+  // GLX info
+  std::cout <<
+#if defined(GLX_VERSION_1_1)
+    "GLX server vendor: " << glXQueryServerString(dsp, scn, GLX_VENDOR) <<
+      "\n" <<
+    "GLX server version: " << glXQueryServerString(dsp, scn, GLX_VERSION) <<
+      "\n" <<
+    "GLX server extensions: " << glXQueryServerString(dsp, scn, GLX_EXTENSIONS) <<
+      "\n" <<
+    "GLX client vendor: " << glXGetClientString(dsp, GLX_VENDOR) << "\n" <<
+    "GLX client version: " << glXGetClientString(dsp, GLX_VERSION) << "\n" <<
+    "GLX client extensions: " << glXGetClientString(dsp, GLX_EXTENSIONS) <<
+      "\n" <<
 #else
-  std::cout << "not implemented" << std::endl;
+    "GLX client/server information excluded (GLX version < 1.1)" <<
+#endif  // !defined(GLX_VERSION_1_1)
+    std::flush;
+  // TODO: still need to choose pixel format, create OpenGL context, etc.
+  // done, close display
+  // TODO: handle possible BadGC error
+  XCloseDisplay(dsp);
 #endif  // !defined(_WIN32)
   return EXIT_SUCCESS;
 }
