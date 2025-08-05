b6b551fcce10a4fc5591b653ad3f32793997ada9
Derek Huang <djh458@stern.nyu.edu>
2025-08-05 03:20:32 -0400

    opengl_info.cc: add simple argument parsing to support not printing OpenGL/GLX extensions

--- src/opengl_info.cc
+++ src/opengl_info.cc
@@ -29,6 +29,7 @@
 #include <cstdlib>
 #include <filesystem>
 #include <iostream>
+#include <string_view>
 
 #include <GL/gl.h>
 // TODO: should have separate check for GLX
@@ -36,6 +37,7 @@
 #include <GL/glx.h>
 #endif  // PDMPMT_HAS_X11
 
+#include "pdmpmt/features.h"
 #include "pdmpmt/opengl.hh"
 #ifdef _WIN32
 #include "pdmpmt/win32.hh"
@@ -43,7 +45,80 @@
 
 namespace {
 
-const auto progname = std::filesystem::path{__FILE__}.string();
+// program name and usage
+const auto progname = std::filesystem::path{__FILE__}.stem().string();
+const auto program_usage = "Usage: " + progname + " [-h] [--nx-gl]"
+#if PDMPMT_HAS_X11
+  " [--nx-glx]"
+#endif  // PDMPMT_HAS_X11
+  "\n"
+  "\n"
+  "Print information on available OpenGL runtime.\n"
+  "\n"
+  "This includes the OpenGL supported version, vendor, renderer, and any\n"
+  "extensions supported by the implementation. For GLX-based implementations,\n"
+  "additional information on the X and GLX client/server are included.\n"
+  "\n"
+  "Options:\n"
+  "  -h, --help             Print this usage\n"
+  "  --nx-gl                Do not print the supported OpenGL extensions"
+#if PDMPMT_HAS_X11
+  "\n"
+  "  --nx-glx               Do not print the supported GLX extensions"
+#endif  // PDMPMT_HAS_X11
+  ;
+
+/**
+ * Command-line options struct.
+ *
+ * @param print_usage Print the program usage
+ * @param print_gl_ext Print the supported OpenGL extensions
+ * @param pring_glx_ext Print the supported GLX extensions
+ */
+struct cli_options {
+  bool print_usage = false;
+  bool print_gl_ext = true;
+  // TODO: maybe only include if GLX is being used
+  bool print_glx_ext = true;
+};
+
+/**
+ * Parse the incoming command-line arguments.
+ *
+ * @param opts Command-line options to set
+ * @param argc Argument couunt from `main()`
+ * @param argv Argument vector from `main()`
+ * @returns `true` if parsing suceeded, `false` otherwise
+ */
+bool parse_args(cli_options& opts, int argc, char** argv)
+{
+  // loop through arguments
+  for (int i = 1; i < argc; i++) {
+    // current argument
+    std::string_view arg{argv[i]};
+    // help option (break early)
+    if (arg == "-h" || arg == "--help") {
+      opts.print_usage = true;
+      return true;
+    }
+    // --nx-gl
+    else if (arg == "--nx-gl")
+      opts.print_gl_ext = false;
+#if PDMPMT_HAS_X11
+    // --nx-glx
+    else if (arg == "--nx-glx")
+      opts.print_glx_ext = false;
+#endif  // PDMPMT_HAS_X11
+    // unknown option
+    else {
+      std::cerr << "Error: Unknown option " << arg << ". Try " << progname <<
+        " --help for usage" << std::endl;
+      return false;
+    }
+  }
+  // done
+  return true;
+}
 
 /**
  * Print OpenGL platform and device info to standard output.
@@ -51,23 +126,36 @@ const auto progname = std::filesystem::path{__FILE__}.string();
  * This function encapsulates the actual OpenGL informational operations we
  * want to perform. All the platform-dependent OpenGL context creation and
  * cleanup logic is handled outside of this subroutine.
+ *
+ * @param print_ext `true` to print OpenGL extensions
  */
-void print_info()
+void print_info(bool print_ext = true)
 {
   std::cout <<
     "OpenGL version: " << glGetString(GL_VERSION) << "\n" <<
     "OpenGL vendor: " << glGetString(GL_VENDOR) << "\n" <<
-    "OpenGL renderer: " << glGetString(GL_RENDERER) << "\n" <<
-    // TODO: re-enable later if user supplies command-line flag. there can be
-    // a large number of extensions for the device and it becomes very verbose
-    "OpenGL extensions: " << pdmpmt::opengl::extensions << "\n" <<
-    std::flush;
+    "OpenGL renderer: " << glGetString(GL_RENDERER) << "\n";
+  // only print extensions if requested. this is because there can be a large
+  // number of extensions for the device and output gets very verbose
+  if (print_ext)
+    std::cout << "OpenGL extensions: " << pdmpmt::opengl::extensions << "\n";
+  // done, so flush
+  std::cout << std::flush;
 }
 
 }  // namespace
 
-int main()
+int main(int argc, char** argv)
 {
+  // parse command-line args
+  cli_options opts;
+  if (!parse_args(opts, argc, argv))
+    return EXIT_FAILURE;
+  // print usage
+  if (opts.print_usage) {
+    std::cout << program_usage << std::endl;
+    return EXIT_SUCCESS;
+  }
 #if defined(_WIN32)
   // register window class. we don't care about the icon
   WNDCLASSA wclass{};
@@ -133,7 +221,7 @@ int main()
   pdmpmt::opengl::context glc{dc, true};
   pdmpmt::opengl::init_extensions();
   // print OpenGL info
-  print_info();
+  print_info(opts.print_gl_ext);
 #elif PDMPMT_HAS_X11
   // open default X display (e.g. :0 on WSL1 from the DISPLAY env var)
   auto dsp = XOpenDisplay(nullptr);
@@ -171,17 +259,25 @@ int main()
     "GLX server vendor: " << glXQueryServerString(dsp, scn, GLX_VENDOR) <<
       "\n" <<
     "GLX server version: " << glXQueryServerString(dsp, scn, GLX_VERSION) <<
-      "\n" <<
-    "GLX server extensions: " << glXQueryServerString(dsp, scn, GLX_EXTENSIONS) <<
-      "\n" <<
+      "\n";
+  // only print extensions if requested
+  if (opts.print_glx_ext)
+    std::cout <<
+      "GLX server extensions: " <<
+        glXQueryServerString(dsp, scn, GLX_EXTENSIONS) << "\n";
+  std::cout <<
     "GLX client vendor: " << glXGetClientString(dsp, GLX_VENDOR) << "\n" <<
-    "GLX client version: " << glXGetClientString(dsp, GLX_VERSION) << "\n" <<
-    "GLX client extensions: " << glXGetClientString(dsp, GLX_EXTENSIONS) <<
-      "\n" <<
+    "GLX client version: " << glXGetClientString(dsp, GLX_VERSION) << "\n";
+  // only print extensions if requested
+  if (opts.print_glx_ext)
+    std::cout <<
+      "GLX client extensions: " << glXGetClientString(dsp, GLX_EXTENSIONS) <<
+      "\n";
 #else
-    "GLX client/server information excluded (GLX version < 1.1)" <<
+    "GLX client/server information excluded (GLX version < 1.1)\n";
 #endif  // !defined(GLX_VERSION_1_1)
-    std::flush;
+  // fully flush output
+  std::cout << std::flush;
   // choose pixel format. this is as close to the Windows format as posible
   int px_fmt[] = {GLX_RGBA, GLX_DOUBLEBUFFER, GLX_DEPTH_SIZE, 16, None};
   auto xvi = glXChooseVisual(dsp, scn, px_fmt);
@@ -205,7 +301,8 @@ int main()
   }
   // initialize OpenGL extensions + print info
   pdmpmt::opengl::init_extensions();
-  print_info();
+  // print OpenGL info
+  print_info(opts.print_gl_ext);
   // explicitly make context non-current + destroy GLX context
   // TODO: handle GLXBadContext and glXMakeCurrent errors. unclear whether it
   // is absolutely necessary to call glXMakeCurrent (see glXDestroyContext)
