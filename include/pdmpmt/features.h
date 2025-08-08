c778c63726bdcfb2a301c620466e65ce48084f10
Derek Huang <djh458@stern.nyu.edu>
2025-08-08 19:39:07 -0400

    features.h: add PDMPMT_HAS_X11 and PDMPMT_HAS_GLX feature macros

--- include/pdmpmt/features.h
+++ include/pdmpmt/features.h
@@ -17,7 +17,6 @@
 #endif  // PDMPMT_CPLUSPLUS >= 202002L
 #endif  // PDMPMT_CPLUSPLUS
 
-// TODO: use CXX macros and remove CC ones
 #ifndef PDMPMT_HAS_CXX20
 #define PDMPMT_HAS_CXX20 0
 #endif  // PDMPMT_HAS_CXX20
@@ -25,5 +24,28 @@
 // compatibility
 // TODO: refactor uses of the compatibility macros
 #define PDMPMT_HAS_CC20 PDMPMT_HAS_CXX20
+
+// X11
+#ifdef __has_include
+#if __has_include(<X11/Xlib.h>)
+#define PDMPMT_HAS_X11 1
+#endif  // __has_include(<X11/Xlib.h>)
+#endif  // __has_include
+
+#ifndef PDMPMT_HAS_X11
+#define PDMPMT_HAS_X11 0
+#endif  // PDMPMT_HAS_X11
+
+// GLX
+// note: usually GL/glx.h includes X11/Xlib.h itself
+#ifdef __has_include
+#if __has_include(<GL/glx.h>)
+#define PDMPMT_HAS_GLX 1
+#endif  // __has_include(<GL/glx.h>)
+#endif  // __has_include
+
+#ifndef PDMPMT_HAS_GLX
+#define PDMPMT_HAS_GLX 0
+#endif  // PDMPMT_HAS_GLX
 
 #endif  // PDMPMT_FEATURES_H_
