e30c1987d7ae9b753f8b356c2a1e99a5539e7f29
Derek Huang <djh458@stern.nyu.edu>
2025-11-23 03:00:17 -0500

    build.sh: ensure last command status is propagated

--- build.sh
+++ build.sh
@@ -135,7 +135,8 @@ main () {
         cmake -S . -B $BUILD_OUTPUT_DIR -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
             $CMAKE_ARGS && cmake --build $BUILD_OUTPUT_DIR -j $CMAKE_BUILD_ARGS
     fi
-    return 0
+    # ensure we propagate the status of last command
+    return $?
 }
 
 main "$@"
