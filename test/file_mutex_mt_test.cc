5fafc4d724263fb24f87a11e47cb0d67cba3c7e3
Derek Huang <djh458@stern.nyu.edu>
2026-03-20 03:33:07 -0400

    add file_mutex_mt_test.cc to test inter-thread file_mutex synchronization
    modified:   test/CMakeLists.txt
    new file:   test/file_mutex_mt_test.cc

---
+++ test/file_mutex_mt_test.cc
@@ -0,0 +1,60 @@
+/**
+ * @file file_mutex_mt_test.cc
+ * @author Derek Huang
+ * @brief C++ program testing `file_mutex` thread synchronization
+ * @copyright MIT License
+ */
+
+#include <cstdlib>
+#include <filesystem>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <unordered_set>
+#include <vector>
+
+#include "pdmpmt/file_mutex.hh"
+
+int main()
+{
+  // thread count
+  // TODO: enable this to be specified through command-line later
+  constexpr auto n_threads = 250u;
+  // set of thread IDs collected during run
+  std::unordered_set<std::thread::id> tids;
+  // get lockfile path + ensure any previous lockfile is removed
+  auto path = std::filesystem::temp_directory_path() / "file_mutex_mt_test.lock";
+  if (std::filesystem::remove(path))
+    std::cerr << "lockfile " << path << " removed" << std::endl;
+  // create mutex + print lockfile location
+  pdmpmt::file_mutex mut{path};
+  std::cout << "lockfile " << path << " created" << std::endl;
+  // launch threads inserting IDs into tids
+  std::cout << "spawning " << n_threads << " threads... " << std::flush;
+  {
+    std::vector<std::thread> threads(n_threads);
+    // start threads
+    for (auto& thread : threads)
+      thread = std::thread{
+        [&mut, &tids]
+        {
+          std::lock_guard _{mut};
+          tids.insert(std::this_thread::get_id());
+        }
+      };
+    // join
+    for (auto& thread : threads)
+      thread.join();
+  }
+  std::cout << "done" << std::endl;
+  // tids should have size n_threads
+  if (tids.size() == n_threads) {
+    std::cout << "result:   OK"  << std::endl;
+    return EXIT_SUCCESS;
+  }
+  else {
+    std::cout << "result: FAIL (expected " << n_threads << " != actual " <<
+      tids.size() << std::endl;
+    return EXIT_FAILURE;
+  }
+}
