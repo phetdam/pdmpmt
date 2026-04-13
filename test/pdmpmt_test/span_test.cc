06ca4a9225003c84ea5fa3b963b5696524080339
Derek Huang <djh458@stern.nyu.edu>
2026-04-12 23:32:18 -0400

    span_test.cc: add vector tests

--- test/pdmpmt_test/span_test.cc
+++ test/pdmpmt_test/span_test.cc
@@ -7,9 +7,16 @@
 
 #include "pdmpmt/span.hh"
 
+#include <algorithm>
 #include <type_traits>
 
 #include <gtest/gtest.h>
+
+#include "pdmpmt/features.h"
+
+#if PDMPMT_HAS_GMOCK
+#include <gmock/gmock.h>
+#endif  // PDMPMT_HAS_GMOCK
 
 namespace {
 
@@ -25,6 +32,42 @@ TEST_F(SpanTest, AddConstTest)
 {
   using pdmpmt::span;
   EXPECT_TRUE((std::is_convertible_v<span<float>, span<const float>>));
+}
+
+/**
+ * Test span modification.
+ */
+TEST_F(SpanTest, ModifyTest)
+{
+  std::vector v1{1, 2, 3, 4, 5};
+  std::vector v2{0, 1, 2, 3, 4};
+  pdmpmt::span s2{v2.data(), v2.size()};
+  for (auto& v : s2)
+    v += 1;
+#if PDMPMT_HAS_GMOCK
+  EXPECT_THAT(v2, ::testing::Pointwise(::testing::Eq(), v1));
+#else
+  EXPECT_EQ(v1, v2);
+#endif  // !PDMPMT_HAS_GMOCK
+}
+
+/**
+ * Test span integration with `std::vector`.
+ */
+TEST_F(SpanTest, VectorTest)
+{
+  std::vector vec{1, 2, 3, 4, 5};
+  pdmpmt::span view{vec.data(), vec.size()};
+#if PDMPMT_HAS_GMOCK
+  // note: EXPECT_THAT works with different containers with the same element
+  // type as long as they provide the value_type member
+  EXPECT_THAT(view, ::testing::Pointwise(::testing::Eq(), vec));
+#else
+  // copy into vector for comparison
+  decltype(vec) vec2(vec.size());
+  std::copy(view.begin(), view.end(), vec2.begin());
+  EXPECT_EQ(vec, vec2);
+#endif  // !PDMPMT_HAS_GMOCK
 }
 
 }  // namespace
