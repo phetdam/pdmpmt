838c260f49a7492bfa8dadaf97e8196907106fa6
Derek Huang <djh458@stern.nyu.edu>
2026-07-26 01:09:53 -0400

    file_mutex.hh: fix comment

--- include/pdmpmt/file_mutex.hh
+++ include/pdmpmt/file_mutex.hh
@@ -91,7 +91,7 @@ public:
    * monitoring the directory containing the lockfile for file deletion events.
    * If the received event corresponds to the lockfile being deleted, then we
    * `try_lock()` again, and if unsuccessful, block again to continue polling
-   * the inotify descriptor for more file deletion events. As one migh expect,
+   * the inotify descriptor for more file deletion events. As one might expect,
    * using inotify is much more efficient than using a spinlock, as the latter
    * produces more volatile timings and consumes much more user + kernel CPU.
    *
